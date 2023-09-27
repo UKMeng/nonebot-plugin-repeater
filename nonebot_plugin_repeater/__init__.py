d4f0302b0d35fab717004bff8ea50b0dbd04f9d3
UKMeng <ukmengse@gmail.com>
2023-09-27 21:52:48 +0800

    ✨ feat(main): 适配red，其他适配器可能也能用

--- nonebot_plugin_repeater/__init__.py
+++ nonebot_plugin_repeater/__init__.py
@@ -1,7 +1,12 @@
 import re
 
-from nonebot import on_message, logger
-from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent
+from nonebot import on_message, logger, require
+
+require("nonebot_plugin_session")
+require("nonebot_plugin_alconna")
+
+from nonebot.adapters import Bot, Event
+from nonebot_plugin_session import extract_session, SessionLevel
 
 from . import config
 
@@ -9,43 +14,62 @@ repeater_group = config.repeater_group
 shortest = config.shortest_length
 blacklist = config.blacklist
 
+
 m = on_message(priority=10, block=False)
+
 
 last_message = {}
 message_times = {}
 
 
 # 消息预处理
-def message_preprocess(message: str):
-    raw_message = message
-    contained_images = {}
-    images = re.findall(r'\[CQ:image.*?]', message)
-    for i in images:
-        contained_images.update({i: [re.findall(r'url=(.*?)[,\]]', i)[0][0], re.findall(r'file=(.*?)[,\]]', i)[0][0]]})
-    for i in contained_images:
-        message = message.replace(i, f'[{contained_images[i][1]}]')
-    return message, raw_message
+def message_preprocess(raw_message):
+    message = ""
+    for seg in raw_message:
+        if seg.type == "image":
+            img = list(seg.values())[1]
+            if "md5" in img:
+                message += img["md5"]
+            elif "file" in img:
+                message += img["file"]
+            else:
+                raise ValueError("可能还没有适配，请提交issue或pr T_T")
+            
+            logger.debug(f"图片 {list(seg.values())[1]}")
+        elif seg.type == "text":
+            message += seg
 
+    logger.debug(f"总 {message}")
+    return message
 
 @m.handle()
-async def repeater(bot: Bot, event: GroupMessageEvent):
-    # 检查是否在黑名单中
-    if event.raw_message in blacklist:
-        logger.debug(f'[复读姬] 检测到黑名单消息: {event.raw_message}')
-        return
-    gid = str(event.group_id)
-    if gid in repeater_group or "all" in repeater_group:
-        global last_message, message_times
-        message, raw_message = message_preprocess(str(event.message))
-        logger.debug(f'[复读姬] 这一次消息: {message}')
-        logger.debug(f'[复读姬] 上一次消息: {last_message.get(gid)}')
-        if last_message.get(gid) != message:
-            message_times[gid] = 1
-        else:
-            message_times[gid] += 1
-        logger.debug(f'[复读姬] 已重复次数: {message_times.get(gid)}/{config.shortest_times}')
-        if message_times.get(gid) == config.shortest_times:
-            logger.debug(f'[复读姬] 原始的消息: {str(event.message)}')
-            logger.debug(f"[复读姬] 欲发送信息: {raw_message}")
-            await bot.send_group_msg(group_id=event.group_id, message=raw_message, auto_escape=False)
-        last_message[gid] = message
+async def repeater(bot: Bot, event: Event):
+    session = extract_session(bot, event)
+    logger.debug(session)
+    # 如果是私聊，跳过
+    if(session.level != SessionLevel.LEVEL2):
+        logger.debug(f"非群聊消息，跳过")
+        m.finish()
+    else:
+        raw_message = event.message
+        str_message = str(event.get_message())
+        # 检查是否在黑名单中
+        if str_message in blacklist:
+            logger.debug(f'[复读姬] 检测到黑名单消息: {str_message}')
+            return
+        gid = str(session.id2)
+        if gid in repeater_group or "all" in repeater_group:
+            global last_message, message_times
+            message = message_preprocess(raw_message)
+            logger.debug(f'[复读姬] 这一次消息: {message}')
+            logger.debug(f'[复读姬] 上一次消息: {last_message.get(gid)}')
+            if last_message.get(gid) != message:
+                message_times[gid] = 1
+            else:
+                message_times[gid] += 1
+            logger.debug(f'[复读姬] 已重复次数: {message_times.get(gid)}/{config.shortest_times}')
+            if message_times.get(gid) == config.shortest_times:
+                logger.debug(f'[复读姬] 原始的消息: {message}')
+                logger.debug(f"[复读姬] 欲发送信息: {raw_message}")
+                await m.send(raw_message)
+            last_message[gid] = message
